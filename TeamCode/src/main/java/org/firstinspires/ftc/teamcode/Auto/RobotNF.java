8ee7722e4ad2f8ad7ebd30e11f766edec40dfcd7
PranavDharanipathy <pranavsdharanipathy@gmail.com>
2026-03-04 21:22:19 -0600

    sm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RobotNF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RobotNF.java
@@ -1,4 +1,124 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-public class RobotNF {
+import com.pedropathing.paths.PathChain;
+
+import org.firstinspires.ftc.teamcode.Auto.Subsystems.FlywheelNF;
+import org.firstinspires.ftc.teamcode.Auto.Subsystems.HoodNF;
+import org.firstinspires.ftc.teamcode.Auto.Subsystems.IntakeNF;
+import org.firstinspires.ftc.teamcode.Auto.Subsystems.TransferNF;
+import org.firstinspires.ftc.teamcode.Auto.Subsystems.TurretNF;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.delays.WaitUntil;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.core.subsystems.SubsystemGroup;
+
+/// He da big R, big R for Robot
+public class RobotNF extends SubsystemGroup {
+
+
+    private RobotNF() {
+
+        super(
+                IntakeNF.INSTANCE,
+                TransferNF.INSTANCE,
+                HoodNF.INSTANCE,
+                TurretNF.INSTANCE,
+                FlywheelNF.INSTANCE
+        );
+    }
+
+    public static final RobotNF robot = new RobotNF();
+
+    //transfer
+    public final Command shootBalls(double transferTime, double timeBetweenTransfers) {
+
+        return new SequentialGroup(
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong(),
+
+                new Delay(timeBetweenTransfers),
+
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong(),
+
+                new Delay(timeBetweenTransfers),
+
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong()
+        );
+    }
+
+    public final Command shootBallsAtParametricEnd(double transferTime, double timeBetweenTransfers, PathChain pathChain) {
+
+        return new SequentialGroup(
+                new WaitUntil(() -> pathChain.lastPath().isAtParametricEnd()),
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong(),
+
+                new Delay(timeBetweenTransfers),
+
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong(),
+
+                new Delay(timeBetweenTransfers),
+
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong()
+        );
+    }
+
+    public final Command shootBalls(double transferTime, double timeBetweenTransfers, double distance, PathChain pathChain) {
+
+        return new SequentialGroup(
+                new WaitUntil(() -> pathChain.lastPath().getDistanceRemaining() <= distance),
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong(),
+
+                new Delay(timeBetweenTransfers),
+
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong(),
+
+                new Delay(timeBetweenTransfers),
+
+                TransferNF.INSTANCE.transfer(),
+                new Delay(transferTime),
+                TransferNF.INSTANCE.antiStrong()
+        );
+    }
+
+    //hood
+    public final Command hoodTo(double position) {
+        return new InstantCommand(() -> HoodNF.INSTANCE.setPosition(position));
+    }
+
+    //intake
+
+    /// Intakes first and then outtakes
+    public final Command intakeClearingSpecial(double outtakeTime) {
+
+        return new SequentialGroup(
+
+                IntakeNF.INSTANCE.fullReverse(),
+                new Delay(outtakeTime),
+                IntakeNF.INSTANCE.intake()
+        );
+    }
+
+    public final void end() {
+        IntakeNF.INSTANCE.end();
+        TransferNF.INSTANCE.end();
+        FlywheelNF.INSTANCE.end();
+    }
 }
